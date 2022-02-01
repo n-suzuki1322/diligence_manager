3f09f5f88abdbbc0f0d0b385ee17de49e5cde1f6
n-suzuki <n-suzuki@msd-net.co.jp>
2022-02-01 12:05:34 +0900

    [temp] develop port番号修正

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const bcrypt = require("bcrypt");
 const app = express();
-const port = process.env.PORT || 4649;
+const port = 4649;
 const dotenv = require("dotenv");
 dotenv.config();
 
